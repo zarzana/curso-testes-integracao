0c108717535d299b8cb37d9e4787164f588ce7b8
Gabriel Zarzana Espinoza <gabriel.zarzana@gmail.com>
2023-09-27 18:00:15 -0300

    Exercise 08

--- aula-testes-08/tests/app.test.ts
+++ aula-testes-08/tests/app.test.ts
@@ -1,22 +1,22 @@
 import supertest from "supertest";
-
 import app from "./../src/app";
 import { ReservationInput } from "../src/repository";
+import prisma from "database";
 
 const api = supertest(app);
 
-describe("API test", () => {
-  it("should create a reservation", async () => {
-    const reservation: ReservationInput = {
-      startDate: new Date(),
-      endDate: new Date()
-    };
+beforeAll(async () => {
+  await prisma.reservation.deleteMany();
+})
 
+describe("API testing", () => {
+  it("POST /reservations should created a new reservation and return status 201", async () => {
+    const reservation: ReservationInput = { startDate: new Date(), endDate: new Date() };
     const { status } = await api.post("/reservations").send(reservation);
     expect(status).toBe(201);
   });
 
-  it("should return all reservations", async () => {
+  it("GET /reservations should return status 200 and a list of all reservations", async () => {
     const { status, body } = await api.get("/reservations");
     expect(status).toBe(200);
     expect(body).toHaveLength(1);
