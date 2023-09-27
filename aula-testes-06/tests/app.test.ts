77da853a9e9c0a7a96980919ce1d3bf636454597
Gabriel Zarzana Espinoza <gabriel.zarzana@gmail.com>
2023-09-27 17:51:48 -0300

    Exercise 06

--- aula-testes-06/tests/app.test.ts
+++ aula-testes-06/tests/app.test.ts
@@ -4,6 +4,15 @@ import app from "./../src/app";
 
 const api = supertest(app);
 
-describe("API test", () => {
-  // TODO
+describe("API testing", () => {
+  it("/event should return status 200 alongise correct body", async () => {
+    const { status, body } = await api.get('/event');
+    expect(status).toBe(200);
+    expect(body).toEqual(expect.objectContaining({
+      id: expect.any(Number),
+      title: expect.any(String),
+      image: expect.any(String),
+      date: expect.any(String)
+    }))
+  })
 });
